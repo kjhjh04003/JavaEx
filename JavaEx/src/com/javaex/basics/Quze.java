9e87aac4b1cb8d46030bd2960877bc6c00b3cb6a
김준형 <kjhjh04003@naver.com>
2021-05-06 08:49:20 +0900

    퀴즈 수정

--- JavaEx/src/com/javaex/basics/Quze.java
+++ JavaEx/src/com/javaex/basics/Quze.java
@@ -9,17 +9,18 @@ public class Quze {
 		//1. 구구단2~9단까지
 		//2. 별모야 1~6까지
 		forEx();
-		whileEx();
+		//whileEx();
 	}
 	private static void forEx() {
 		//for문이용 구구단
+	/*
 			for(int i=2;i<=9;i++) {
 				for(int j=1;j<=9;j++) {
 					System.out.println(i + "*" + j + "=" + i*j);
 				}
 				System.out.println();
 			}
-			
+			*/
 			//for문 이용 별모양
 			for(int i=0;i<6;i++) {
 				for(int j=0;j<=i;j++) {
