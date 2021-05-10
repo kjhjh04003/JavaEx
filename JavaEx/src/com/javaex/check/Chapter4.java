1bb867efc9f2a328dbba76609433ee4292d2894b
김준형 <kjhjh04003@naver.com>
2021-05-10 13:01:13 +0900

    이것이 자바다 4장 확인문제

---
+++ JavaEx/src/com/javaex/check/Chapter4.java
@@ -0,0 +1,45 @@
+package com.javaex.check;
+
+public class Chapter4 {
+
+	public static void main(String[] args) {
+		problem1();
+		problem2();
+		problem3();
+		problem4();
+		problem5();
+		problem6();
+
+	}
+
+	private static void problem6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void problem5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void problem4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void problem3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void problem2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void problem1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
